7f04e0bdfc4f9bd4a6aa6c1de5909d6511253fc5
yassinerahhou <yassine.rahhou.1@gmail.comm>
2023-06-22 13:07:29 -0700

    xx

--- monty.h
+++ monty.h
@@ -29,25 +29,15 @@ void push(stack_t **stack, unsigned int nbr);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 int is_valid_data(char *data);
-bool digits_check(char *str);
-int execute_line(stack_t **head, char *op_code,
-		char *data, unsigned int line_number);
-void check_num_args(int argc);
-FILE *open_file(char *fileName);
-void free_stack(stack_t **head);
-void error_handler(stack_t **head, int errno,
+void error_h1(stack_t **head, int errno,
 		unsigned int line_num, char *line, char *op_code);
-void (*select_op(char *s))(stack_t **, unsigned int);
-
 
 #endif
